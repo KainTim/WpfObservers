ab057dcda5cc7ccf8c4d322dacbf72ed6eb792f0
Tim Kainz <tikaiz@gmx.at>
2025-03-27 12:01:54 +0100

    Add project files.

---
+++ WpfObservers/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfObservers;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+  private void btnAddClock_Click(object sender, RoutedEventArgs e)
+  {
+    
+    new Window.Show();
+  }
+
+  private void btnStartTimer_Click(object sender, RoutedEventArgs e)
+  {
+
+  }
+}
